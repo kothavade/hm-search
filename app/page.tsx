bebf31e6b671e41e06dc5c72e1c68f1089e224fd
Ved Kothavade <ved@kothavade.com>
2023-12-22 18:51:52 -0500

    rm commented code

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,6 @@ import { DataTable } from "@/components/table/table"
 export default function IndexPage() {
   return (
     <section className="container">
-      {/* <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          {siteConfig.description}
-        </h1>
-      </div> */}
       <DataTable
         columns={columns}
         data={options.options as unknown as Record[]}
